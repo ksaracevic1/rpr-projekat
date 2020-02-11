0075a18be35349932cd2c7ac5365d32272e31532
ksaracevic1 <ksaracevic1@etf.unsa.ba>
2020-02-11 20:13:36 +0100

    Interface changes

---
+++ src/ba/unsa/etf/rpr/projekat/Interfaces/DatabaseDAO.java
@@ -0,0 +1,34 @@
+package ba.unsa.etf.rpr.projekat.Interfaces;
+
+import ba.unsa.etf.rpr.projekat.DTO.AdminAccount;
+import ba.unsa.etf.rpr.projekat.DTO.Developer;
+import ba.unsa.etf.rpr.projekat.DTO.UserAccount;
+import ba.unsa.etf.rpr.projekat.DTO.VideoGame;
+import javafx.collections.ObservableList;
+
+public interface DatabaseDAO {
+    ObservableList<VideoGame> getVideoGames();
+    void addVideoGame(VideoGame videoGame);
+    void removeVideoGame(VideoGame videoGame);
+    void updateVideoGame(VideoGame videoGame);
+    VideoGame getVideoGameById(int id);
+
+    ObservableList<Developer> getDeveloper();
+    void addDeveloper(Developer developer);
+    void removeDeveloper(Developer developer);
+    void updateDeveloper(Developer developer);
+    Developer getDeveloperById(int id);
+
+    ObservableList<UserAccount> getUsers();
+    void addUser(UserAccount userAccount);
+    void removeUser(UserAccount userAccount);
+    void updateUser(UserAccount userAccount);
+    UserAccount getUserById(int id);
+
+    ObservableList<AdminAccount> getAdmin();
+    void addAdmin(AdminAccount adminAccount);
+    void removeAdmin(AdminAccount adminAccount);
+    void updateAdmin(AdminAccount adminAccount);
+    AdminAccount getAdminById(int id);
+    void close();
+}
