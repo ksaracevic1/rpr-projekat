457473b1c4114ece750f4313575ffa52b521c539
ksaracevic1 <ksaracevic1@etf.unsa.ba>
2020-02-17 01:17:48 +0100

    Adding methods to controllers

--- src/ba/unsa/etf/rpr/projekat/Controllers/LoginController.java
+++ src/ba/unsa/etf/rpr/projekat/Controllers/LoginController.java
@@ -22,89 +22,74 @@ import java.util.ResourceBundle;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
-public class LoginController extends Controller{
+public class LoginController extends Controller {
     public TextField usernameField;
     public PasswordField passwordField;
 
-    public Button okButton;
-    public Button exitButton;
+    public Button loginButton, exitButton, registerButton;
 
     public Label errorLabel;
 
     private DatabaseDAO dao = null;
-    private ArrayList<Account> accounts=null;
+    private ArrayList<Account> accounts = null;
 
-    public LoginController(DatabaseDAO dao){
-        this.dao=dao;
+    public LoginController(DatabaseDAO dao) {
+        this.dao = dao;
     }
 
     @FXML
-    public void initialize(){
+    public void initialize() {
         if (dao != null) dao.close();
         dao = new DatabaseDAODB();
-        accounts=new ArrayList<Account>();
+        accounts = new ArrayList<Account>();
         accounts.addAll(dao.getAdmins());
         accounts.addAll(dao.getUsers());
     }
 
-    public void okClick(ActionEvent actionEvent){
+    public void loginClick(ActionEvent actionEvent) {
         errorLabel.setText("");
-        String un=usernameField.getText();
-        String pass=passwordField.getText();
-        Boolean exists=false;
-        if(!un.equals("") && !pass.equals("")) {
+        String un = usernameField.getText();
+        String pass = passwordField.getText();
+        Boolean exists = false;
+        if (!un.equals("") && !pass.equals("")) {
             for (int i = 0; i < accounts.size(); i++) {
-                Account currentAccount=accounts.get(i);
-                if(un.equals(currentAccount.getUsername())){
-                    exists=true;
-                    if(currentAccount.getPassword().equals(pass)){
+                Account currentAccount = accounts.get(i);
+                if (un.equals(currentAccount.getUsername())) {
+                    exists = true;
+                    if (currentAccount.getPassword().equals(pass)) {
                         openMain(currentAccount);
                         Stage stage = (Stage) exitButton.getScene().getWindow();
                         stage.close();
-                    }
-                    else
-                    {
+                    } else {
                         errorLabel.setText("Incorrect password");
                     }
                     break;
                 }
             }
-            if(!exists) {
+            if (!exists) {
                 errorLabel.setText("Incorrect username");
             }
-        }
-        else{
+        } else {
             errorLabel.setText("Username or password field empty");
         }
     }
 
-    public void exitClick(ActionEvent actionEvent){
+    public void registerClick(ActionEvent actionEvent){
+        UIControl.openWindow(getClass(),new RegistrationController(dao,accounts),ResourceBundle.getBundle("Language"),"register.fxml");
+    }
+
+    public void exitClick(ActionEvent actionEvent) {
         Stage stage = (Stage) exitButton.getScene().getWindow();
         stage.close();
     }
 
-    public void openMain(Account account){
+    public void openMain(Account account) {
         String viewType;
-        if(account instanceof AdminAccount){
-            viewType="adminView.fxml";
+        if (account instanceof AdminAccount) {
+            viewType = "adminView.fxml";
         } else {
-            viewType="userView.fxml";
+            viewType = "userView.fxml";
         }
-       /* Stage stage = new Stage();
-        Parent root = null;
-        try {
-            ResourceBundle bundle=ResourceBundle.getBundle("Language");
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/"+viewType));
-            MainController mainController = new MainController(dao,account);
-            loader.setController(mainController);
-            loader.setResources(bundle);
-            root = loader.load();
-            stage.setTitle("Video Game Database");
-            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        stage.show();*/
-        UIControl.openWindow(getClass(),new MainController(dao,account),ResourceBundle.getBundle("Language"),viewType);
+        UIControl.openWindow(getClass(), new MainController(dao, account), ResourceBundle.getBundle("Language"), viewType);
     }
 }
--- src/ba/unsa/etf/rpr/projekat/Controllers/MainController.java
+++ src/ba/unsa/etf/rpr/projekat/Controllers/MainController.java
@@ -3,23 +3,32 @@ package ba.unsa.etf.rpr.projekat.Controllers;
 import ba.unsa.etf.rpr.projekat.DAO.DatabaseDAODB;
 import ba.unsa.etf.rpr.projekat.DAO.DatabaseDAOXML;
 import ba.unsa.etf.rpr.projekat.DTO.Account;
+import ba.unsa.etf.rpr.projekat.DTO.Developer;
+import ba.unsa.etf.rpr.projekat.DTO.UserAccount;
 import ba.unsa.etf.rpr.projekat.DTO.VideoGame;
 import ba.unsa.etf.rpr.projekat.Exceptions.InvalidSearchTermException;
 import ba.unsa.etf.rpr.projekat.Interfaces.DatabaseDAO;
+import ba.unsa.etf.rpr.projekat.UIControl;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.TilePane;
+import javafx.stage.Stage;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
-public class MainController extends Controller{
+public class MainController extends Controller {
 
     private DatabaseDAO dao;
     private Account accountInUse;
@@ -32,12 +41,14 @@ public class MainController extends Controller{
         Developer,
     }
 
-
+    public BorderPane mainBorder;
+    public Menu usernameMenu;
     public TextField searchFieldVG, searchFieldDV;
     public ChoiceBox<GameSearchType> choiceBoxVG;
     public TilePane tilePaneVG, tilePaneDV;
     public ObservableList<Button> buttons = FXCollections.observableArrayList();
     public ObservableList<VideoGame> videoGames = FXCollections.observableArrayList();
+    public ObservableList<Developer> developers=FXCollections.observableArrayList();
     public final ObservableList<GameSearchType> searchTypes =
             FXCollections.observableArrayList(
                     GameSearchType.All, GameSearchType.Name, GameSearchType.Genre, GameSearchType.Developer);
@@ -51,6 +62,20 @@ public class MainController extends Controller{
     public void initialize() {
         choiceBoxVG.setItems(searchTypes);
         choiceBoxVG.setValue(GameSearchType.All);
+        usernameMenu.setText(accountInUse.getUsername());
+        if(accountInUse instanceof UserAccount){
+            new Thread(()->{
+                UserAccount ua=(UserAccount) accountInUse;
+                Image image=new Image(ua.getAvatarLink());
+                Platform.runLater(()->{
+                    ImageView imageView=new ImageView(image);
+                    imageView.setFitWidth(15);
+                    imageView.setFitHeight(15);
+                    usernameMenu.setGraphic(imageView);
+                });
+            }).start();
+        }
+
     }
 
 
@@ -65,35 +90,46 @@ public class MainController extends Controller{
     }
 
     public void clickAbout(ActionEvent actionEvent) {
-
+        UIControl.openWindow(getClass(), null, null, "about.fxml");
     }
 
     public void clickLogout(ActionEvent actionEvent) {
-
+        Stage stage = (Stage) mainBorder.getScene().getWindow();
+        stage.close();
     }
 
     public void searchVG(ActionEvent actionEvent) {
         try {
+            GameSearchType selectedType = choiceBoxVG.getValue();
             String search = searchFieldVG.getText();
-            if (search.equals("")) {
+            if (search.equals("") && selectedType!=GameSearchType.All) {
                 throw new InvalidSearchTermException("%searchException");
             }
             videoGames.clear();
-            GameSearchType selectedType = choiceBoxVG.getValue();
             if (selectedType.equals(GameSearchType.All)) {
                 videoGames.setAll(dao.getVideoGames());
             } else if (selectedType.equals(GameSearchType.Name)) {
-
+                videoGames.setAll(dao.getVideoGameByName(search));
             } else if (selectedType.equals(GameSearchType.Developer)) {
-
+                videoGames.setAll(dao.getVideoGameByDeveloper(search));
             } else if (selectedType.equals(GameSearchType.Genre)) {
-
+                videoGames.setAll(dao.getVideoGameByGenre(search));
             }
             for (VideoGame vg : videoGames) {
                 Button button = new Button();
                 button.setOnMouseClicked(event -> {
                     openGameView(vg);
                 });
+                new Thread(()->{
+                    Image image=new Image(vg.getImageLink());
+                    Platform.runLater(()->{
+                        ImageView imageView=new ImageView(image);
+                        imageView.setFitHeight(100);
+                        imageView.setFitWidth(100);
+                        button.setGraphic(imageView);
+                        tilePaneVG.getChildren().add(button);
+                    });
+                }).start();
             }
         } catch (InvalidSearchTermException e) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
@@ -108,7 +144,7 @@ public class MainController extends Controller{
 
     }
 
-    public void openGameView(VideoGame videoGame){
+    public void openGameView(VideoGame videoGame) {
 
     }
 }
