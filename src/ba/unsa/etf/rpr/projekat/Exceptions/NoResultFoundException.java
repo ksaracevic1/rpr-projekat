51d0afcabe51ee9c26dfaff0ecb8d65b0a154d23
ksaracevic1 <ksaracevic1@etf.unsa.ba>
2020-03-01 17:09:32 +0100

    NoResultFoundException exception

---
+++ src/ba/unsa/etf/rpr/projekat/Exceptions/NoResultFoundException.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr.projekat.Exceptions;
+
+public class NoResultFoundException extends Exception {
+
+    public NoResultFoundException(String message) {
+        super(message);
+    }
+}
