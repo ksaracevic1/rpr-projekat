5af84706c34431b026b5ae9ac3c71b55474fa56c
ksaracevic1 <ksaracevic1@etf.unsa.ba>
2020-03-02 15:43:19 +0100

    Button for writing/updating game reviews

--- src/ba/unsa/etf/rpr/projekat/Controllers/GameViewController.java
+++ src/ba/unsa/etf/rpr/projekat/Controllers/GameViewController.java
@@ -12,10 +12,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Font;
 
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -29,11 +32,12 @@ public class GameViewController extends Controller {
     public Label gameDescription;
     public ImageView gameImage;
     public Button reviewButton;
+    public ImageView reviewButtonImageView=new ImageView();
     public ListView<GameReview> reviewListView;
     private VideoGame selectedGame;
     private UserAccount accountInUse;
     private DatabaseDAO dao;
-    private Boolean hasReview=false;
+    private GameReview accountReview=null;
     private ObservableList<GameReview> gameReviews = FXCollections.observableArrayList();
     private ResourceBundle bundle=ResourceBundle.getBundle("Language");
 
@@ -52,6 +56,15 @@ public class GameViewController extends Controller {
         gameReleaseDate.setText(selectedGame.getReleaseDate().format(formatter));
         gameDescription.setText(selectedGame.getDescription());
         UIControl.loadImage(gameImage, selectedGame.getImageLink(), 250, 250);
+        accountReview=dao.getReviewByUserGame(selectedGame,accountInUse);
+        reviewButtonImageView.setFitHeight(16);
+        reviewButtonImageView.setFitWidth(16);
+        reviewButton.setGraphic(reviewButtonImageView);
+        if(accountReview!=null){
+            setButtonUpdate();
+        } else {
+            setButtonWrite();
+        }
         Set<GameReview> gameReviewSet = dao.getReviewsByGameId(selectedGame.getId());
         gameReviews.addAll(gameReviewSet);
         reviewListView.setCellFactory(param -> new ListCell<>() {
@@ -75,4 +88,35 @@ public class GameViewController extends Controller {
         reviewListView.setItems(gameReviews);
     }
 
+    private void setButtonUpdate(){
+        reviewButton.setText(bundle.getString("updateReview"));
+        try {
+            reviewButtonImageView.setImage(new Image(new FileInputStream("resources/img/update.png")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        reviewButton.setOnMouseClicked(event -> {
+            updateReview();
+        });
+    }
+
+    private void setButtonWrite(){
+        reviewButton.setText(bundle.getString("writeReview"));
+        try {
+            reviewButtonImageView.setImage(new Image(new FileInputStream("resources/img/write.png")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        reviewButton.setOnMouseClicked(event -> {
+            writeReview();
+        });
+    }
+
+    private void updateReview(){
+
+    }
+
+    private void writeReview(){
+
+    }
 }
