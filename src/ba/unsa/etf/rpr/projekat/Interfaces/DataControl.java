19029bcf14f819c47b8a85a6960ab198e68db8c6
ksaracevic1 <ksaracevic1@etf.unsa.ba>
2020-03-11 08:51:52 +0100

    DataControl interface for controlling data shown to users

---
+++ src/ba/unsa/etf/rpr/projekat/Interfaces/DataControl.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.projekat.Interfaces;
+
+import javafx.event.ActionEvent;
+
+public interface DataControl {
+    void switchDb(ActionEvent actionEvent);
+    void switchXml(ActionEvent actionEvent);
+    void clearUI();
+}
